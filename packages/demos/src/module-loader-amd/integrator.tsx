3c9c1910966d5291c2f3ed1e646ce9740d8c98a1
Erik Parso <parsoerik@gmail.com>
2022-01-28 14:12:08 +0100

    fuel-savings integrator

--- packages/demos/src/module-loader-amd/integrator.tsx
+++ packages/demos/src/module-loader-amd/integrator.tsx
@@ -1,24 +1,40 @@
-import {createFeatureHub} from '@feature-hub/core';
-import {defineExternals, loadAmdModule} from '@feature-hub/module-loader-amd';
-import {FeatureAppLoader, FeatureHubContextProvider} from '@feature-hub/react';
+import { createFeatureHub } from '@feature-hub/core';
+import { defineExternals, loadAmdModule } from '@feature-hub/module-loader-amd';
+import { FeatureAppLoader, FeatureHubContextProvider } from '@feature-hub/react';
+import * as MaterialUI from '@material-ui/core';
+import * as MaterialUIStyles from '@material-ui/styles';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import '../blueprint-css';
 
-defineExternals({react: React});
+defineExternals({
+	'react': React,
+	'react-dom': ReactDOM,
+	'@material-ui/core': MaterialUI,
+	'@material-ui/styles': MaterialUIStyles
+});
 
-const {featureAppManager} = createFeatureHub('test:integrator', {
-  moduleLoader: loadAmdModule,
-  providedExternals: {react: process.env.REACT_VERSION as string},
+const { featureAppManager } = createFeatureHub('test:integrator', {
+	moduleLoader: loadAmdModule,
+	providedExternals: {
+		react: process.env.REACT_VERSION as string
+	}
 });
 
 ReactDOM.render(
-  <FeatureHubContextProvider value={{featureAppManager}}>
-    <FeatureAppLoader
-      featureAppId="test:hello-world"
-      src="feature-app.umd.js"
-    />
-  </FeatureHubContextProvider>,
-
-  document.querySelector('main')
+	<div style={{ background: 'gray' }}>
+		<FeatureHubContextProvider value={{ featureAppManager }}>
+			<FeatureAppLoader
+				featureAppId="test:fuel-savings"
+				src="https://skoda-fuel-savings-calculator-dev.azurewebsites.net/feature-app/fuel-savings-calculator.min.js"
+				config={{
+					endpoint: 'https://skoda-fuel-savings-calculator-dev.azurewebsites.net',
+					bid: '260',
+					culture: 'cs-cz',
+					isPreview: true
+					// other widget settings
+				}}
+			/>
+		</FeatureHubContextProvider>
+	</div>,
+	document.querySelector('main')
 );
